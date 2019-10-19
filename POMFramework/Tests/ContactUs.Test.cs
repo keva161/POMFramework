028d89f5d783e1833d6ae54d5dcbe3283b232151
Kevin Tuck <keva16@gmail.com>
2019-10-19 17:00:25 +0100

    Cleanup

--- POMFramework/Tests/ContactUs.Test.cs
+++ POMFramework/Tests/ContactUs.Test.cs
@@ -14,7 +14,7 @@ namespace POMFramework
         {
             Pages.Home.GoTo();
             Pages.ContactUs.GoTo();
-            Pages.ContactUs.WaitForPageToLoad();
+            //Pages.ContactUs.WaitForPageToLoad();
             Pages.ContactUs.TypeAMessage();
             Pages.ContactUs.SelectASubject("Webmaster");
             Pages.ContactUs.SubmitForm();
